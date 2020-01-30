1d1f7282f4d6d544bd263788521e12a3a5bd19f0
Gaurav Markan <48889995+gauravmarkan@users.noreply.github.com>
2020-01-30 16:45:05 -0500

    Create GoogleSearch_Object(By_id).java

---
+++ GoogleSearch_Object(By_id).java
@@ -0,0 +1,35 @@
+// This Object is made in accordance with the name taken from the used boxes and search button. 
+// It is clubbed with GoogleSearchTest.
+package pages;
+
+import org.openqa.selenium.By; // Importing the Selenium Library
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+public class GoogleSearchPageObject {
+	
+	WebDriver driver = null; 
+	
+	// Check for the initialised value otherwise the test won't run. 
+	By textbox_search_googleSearch = By.id("lb-tst"); // defining the elements that are utilised by the user - Textbox for Search and to click for searching about the stuff
+			
+	By button_search_googleSearch = By.name("btnK");
+	
+	public GoogleSearchPageObject(WebDriver driver) { 
+		
+	this.driver = driver; 	// This is constructor in order to signify that the same driver is been referred when a code is written. 
+	}
+	
+	public void setTextInSearchBox(String text) { 
+		
+		driver.findElement(textbox_search_googleSearch).sendKeys(text);  // command to perform google search and to write on it with input of some "TEXT "
+		
+	}
+	
+	public void clickSearchButton() {
+		
+		driver.findElement(button_search_googleSearch).sendKeys(Keys.RETURN); // command to click on google search button 
+	}
+	
+
+}
